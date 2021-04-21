46fe9870c471afd7cafa2003ee8fd4e253ab8677
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-21 16:03:08 -0600

    Nest App in Router component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
