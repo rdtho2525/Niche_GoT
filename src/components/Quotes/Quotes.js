bf6446b6ae5749af53594007a406ea085ff67b52
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-22 15:09:21 -0600

    Add initial structure and styling to Quote component

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Quotes.css';
 
-const Quote = () => {
+const Quote = ({ currentQuote, getRandomQuote }) => {
   
   
   return (
-
+    <>
+    <section className="quote-section">
+      <div className="quote-container">
+        <h2>"{currentQuote && currentQuote}"</h2>
+      </div>
+      <div className="button-container">
+        <button>Save this quote?</button>
+        <button onClick={() => {
+          getRandomQuote();
+        }}>Next quote</button>
+      </div>
+    </section>
+    </>
   )
 }
+
+export default Quote;
