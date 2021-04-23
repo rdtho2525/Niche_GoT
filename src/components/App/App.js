d3429bb57c2c9b57eb5a83dc52eb357b487d0124
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-23 16:52:54 -0600

    Add boolean condition to useEffect dependency argument to stop infinite loop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,52 +3,89 @@ import React, { useState, useEffect } from 'react';
 import fetchCharacters from '../../api.js';
 import { getRandomIndex } from '../../utilities.js';
 import Quote from '../Quotes/Quotes.js'
+import Answers from '../Answers/Answers.js';
+import { shuffleCharacters } from '../../utilities.js';
+
 
 function App() {
   const [ characters, setCharacters ] = useState([]);
   const [ quotes, setQuotes ] = useState([]);
   const [ currentQuote, setCurrentQuote ] = useState('');
+  const [ possibleAnswers, setPossibleAnswers ] = useState([]);
   const [ error, setError ] = useState('');
 
   const getCharacters = async () => {
     try {
       const allCharacters = await fetchCharacters();
-      setCharacters(allCharacters);
+      setCharacters(allCharacters)
     } catch(error) {
       setError(error);
       console.log(error)
     }
   }
 
-  const getQuotes= () => {
-    const arr = []
+  const getQuotes = () => {
+    const allQuotes = []
     characters.forEach(character => {
       character.quotes.forEach(quote => {
-        if (!arr.includes(character.quotes)) {
-          arr.push(quote)
+        if (!allQuotes.includes(character.quotes)) {
+          allQuotes.push(quote)
         }
       })
     })
-    setQuotes(arr)
+    // const allQuotes = characters.reduce((arr, character) => {
+    //   console.log(character)
+    //   character.quotes.forEach(quote => {
+    //     if (!arr.includes(character.quotes)) {
+    //       arr.push(quote)
+    //     }
+    //   })
+    // }, [])
+    // console.log(allQuotes)
+    setQuotes(allQuotes)
   }
   
   const getRandomQuote = () => {
     const randomIndex = getRandomIndex(quotes);
     setCurrentQuote(quotes[randomIndex]);
-    console.log(quotes);
+  }
+
+  const getCorrectAnswer = () => {
+    return characters.filter(character => character.quotes.includes(currentQuote))
+  }
+
+  const getIncorrectAnswers = (characters, rightAnswer) => {
+    characters.filter((character, i) => {
+      if (character.name !== rightAnswer.name && i < 3) {
+        return character
+      }
+    })
+  }
+
+  const collectPossibleAnswers = () => {
+    const correctAnswer = getCorrectAnswer()[0];
+    const shuffledCharacters = shuffleCharacters(characters);
+    const incorrectAnswers = getIncorrectAnswers(shuffledCharacters, correctAnswer);
+    // console.log('right answer: ', correctAnswer)
+    // console.log('wrongAnswers: ', incorrectAnswers)
+    // console.log('all answers: ', [...incorrectAnswers, correctAnswer]);
+    incorrectAnswers && setPossibleAnswers(() => [...incorrectAnswers, correctAnswer])
   }
 
   useEffect( async () => {
     await getCharacters();
+  }, []);
+
+  useEffect( async () => {
     await getQuotes();
-  }, [quotes])
+  }, [!!characters.length])
 
   return (
     <>
       {error && <h1>{error}</h1>}
       <Quote currentQuote={currentQuote} getRandomQuote={getRandomQuote}/>
-      {/* {console.log(characters[0])} */}
-      {console.log(quotes)}
+      {/* {console.log(possibleAnswers)} */}
+      {/* {!!possibleAnswers.length && <Answers possibleAnswers={possibleAnswers}/>} */}
     </>
   );
 }
