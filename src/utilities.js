fa13022d0bc1cbb44540ababcc03b8376415d24a
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-23 16:48:28 -0600

    Add shuffleCharacters helper function

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,7 @@
 export const getRandomIndex = (arr) => {
   return Math.floor(Math.random() * arr.length)
 }
+
+export const shuffleCharacters = array => {
+  return array.sort(() => 0.5 - Math.random());
+}
