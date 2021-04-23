e92c9e0893b044d2377950831dd0460a0a0f8429
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-23 16:49:17 -0600

    Hide Link component for possible later use

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './Quotes.css';
 
 const Quote = ({ currentQuote, getRandomQuote }) => {
   
-  
   return (
     <>
-    <section className="quote-section">
-      <div className="quote-container">
-        <h2>"{currentQuote && currentQuote}"</h2>
-      </div>
-      <div className="button-container">
-        <button>Save this quote?</button>
-        <button onClick={() => {
-          getRandomQuote();
-        }}>Next quote</button>
-      </div>
-    </section>
+      <section className="quote-section">
+        <div className="quote-container">
+          <h2>"{currentQuote && currentQuote}"</h2>
+        </div>
+        <div className="button-container">
+          <button>Save this quote?</button>
+          <button onClick={() => {
+            getRandomQuote();
+          }}>Next quote</button>
+        </div>
+      </section>
     </>
   )
 }
