df0edb426aa9203eaabb2234322cbd1db71d570e
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-25 17:26:40 -0600

    Brainstorm save button state, functionality

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Quotes.css';
 
 const Quote = ({ currentQuestion, getRandomQuestion }) => {
-  
+  const [ saveMessage, setSaveMessage ] = useState('Save this quote');
+
+  // const checkSaveStatus = () => {
+  //   if (isSaved) {
+  //     setSaveMessage('Saved ✔')
+  //   } else {
+  //     setSaveMessage('Save this quote ☐')
+  //   }
+  // }
   return (
     <section className="quote-section">
       <div className="quote-container">
         <h2>"{currentQuestion && currentQuestion.quote}"</h2>
       </div>
       <div className="button-container">
-        <button className="post-answer">Save this quote?</button>
+        <button className="post-answer">{saveMessage}</button>
         <button className="post-answer" onClick={() => {
           getRandomQuestion();
         }}>
-          Next quote
+          Next quote ⇨
         </button>
       </div>
     </section>
