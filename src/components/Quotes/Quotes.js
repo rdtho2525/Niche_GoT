2b4ad3bd12108a2fa27a4a773287d49b5ab05445
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-24 19:17:35 -0600

    Add classes to button elements

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -5,19 +5,19 @@ import './Quotes.css';
 const Quote = ({ currentQuestion, getRandomQuestion }) => {
   
   return (
-    <>
-      <section className="quote-section">
-        <div className="quote-container">
-          <h2>"{currentQuestion && currentQuestion.quote}"</h2>
-        </div>
-        <div className="button-container">
-          <button>Save this quote?</button>
-          <button onClick={() => {
-            getRandomQuestion();
-          }}>Next quote</button>
-        </div>
-      </section>
-    </>
+    <section className="quote-section">
+      <div className="quote-container">
+        <h2>"{currentQuestion && currentQuestion.quote}"</h2>
+      </div>
+      <div className="button-container">
+        <button className="post-answer">Save this quote?</button>
+        <button className="post-answer" onClick={() => {
+          getRandomQuestion();
+        }}>
+          Next quote
+        </button>
+      </div>
+    </section>
   )
 }
 
