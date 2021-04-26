e2a6328e85d54f5ec8f609fe1f748ffeb040fbd4
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-26 15:15:32 -0600

    Add getRandomQuestion to Trivia link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import './Footer.css'
 
-const Footer = () => {
+const Footer = ({ getRandomQuestion }) => {
 
   return (
     <footer>
       <Link to="/">Home</Link>
-      <Link to="/trivia">Trivia</Link>
+      <Link to="/trivia" onClick={() => getRandomQuestion()}>Trivia</Link>
       <Link to="/saved-quotes">Saved Quotes</Link>
     </footer>
   )
