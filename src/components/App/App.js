e71cc176d27a2860382eb833ca8aa9a5e95f37be
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-26 14:21:02 -0600

    SRender saved quotes from localStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,9 @@ import fetchCharacters from '../../api.js';
 import { getRandomIndex } from '../../utilities.js';
 import Quote from '../Quotes/Quotes.js'
 import Answers from '../Answers/Answers.js';
+import Footer from '../Footer/Footer.js'
+import SavedQuote from '../SavedQuote/SavedQuote.js'
+import { Switch, Route } from 'react-router-dom';
 import { shuffleCharacters } from '../../utilities.js';
 
 
@@ -13,6 +16,7 @@ function App() {
   const [ currentQuestion, setCurrentQuestion ] = useState('');
   const [ feedback, setFeedback ] = useState('');
   const [ validStatus, setValidStatus ] = useState('');
+  const [ savedQuotes, setSavedQuotes ] = useState([]);
   const [ error, setError ] = useState('');
 
   const getCharacters = async () => {
@@ -32,6 +36,7 @@ function App() {
         character.isCorrect = false;
         arr = [...arr, {answer: character.name, isCorrect: false}]
       }
+
       return arr;
     }, [])
   }
@@ -72,6 +77,12 @@ function App() {
     }
   }
 
+  const saveCurrentQuote = (quote) => {
+    const storedQuotes = JSON.parse(localStorage.getItem('storedQuotes'));
+    storedQuotes ? localStorage.setItem('storedQuotes', JSON.stringify([...storedQuotes, quote])) : localStorage.setItem('storedQuotes', JSON.stringify([quote]));    
+    storedQuotes ? setSavedQuotes([...storedQuotes, quote]) : setSavedQuotes([quote]);
+  }
+
   useEffect(() => {
     getCharacters();
   }, []);
@@ -80,12 +91,36 @@ function App() {
     getQuestions()
   }, [!!characters.length])
 
+  useEffect(() => {
+    const storedQuotes = JSON.parse(localStorage.getItem('storedQuotes'));
+    setSavedQuotes(storedQuotes)
+  }, [])
+
   return (
     <main>
       {error && <h1 className="error-message">{error}</h1>}
-      <Quote currentQuestion={currentQuestion} validStatus={validStatus} getRandomQuestion={getRandomQuestion}/>
-      {feedback && <h2 className={'feedback'}>{feedback}</h2>}
-      {!!currentQuestion.answers && <Answers validateSelection={validateSelection} validStatus={validStatus} possibleAnswers={currentQuestion.answers}/>}
+      <Switch>
+        <Route 
+          exact path="/saved-quotes"
+          render={() => <SavedQuote savedQuotes={savedQuotes}/>}
+        />
+        <Route 
+          exact path="/"
+          render={() => (
+            <>
+              <Quote 
+                currentQuestion={currentQuestion} 
+                validStatus={validStatus} 
+                saveCurrentQuote={saveCurrentQuote} 
+                getRandomQuestion={getRandomQuestion}
+              />
+              {feedback && <h2 className={'feedback'}>{feedback}</h2>}
+              {!!currentQuestion.answers && <Answers validateSelection={validateSelection} validStatus={validStatus} possibleAnswers={currentQuestion.answers}/>}
+            </>
+          )}
+        />
+      </Switch>
+      <Footer />
     </main>
   );
 }
