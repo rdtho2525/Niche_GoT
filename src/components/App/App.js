0b5a1b15259518747b64ecdbab8eb67a357cf5e0
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-26 19:42:00 -0600

    Add isSaved property to quote object

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -51,7 +51,7 @@ function App() {
           var wrongAnswers = getIncorrectAnswers(characters, character)
         } 
         const possibleAnswers = [...wrongAnswers, {answer: character.name,   isCorrect: character.isCorrect}];
-        let question = {quote: quote, answers: shuffleCharacters(possibleAnswers)}
+        let question = {quote: quote, answers: shuffleCharacters(possibleAnswers), isSaved: false}
         arr = [...arr, question]
       })
     
@@ -79,14 +79,16 @@ function App() {
   }
 
   const saveCurrentQuote = (quote) => {
+    quote.isSaved = true;
     const storedQuotes = JSON.parse(localStorage.getItem('storedQuotes'));
     storedQuotes ? localStorage.setItem('storedQuotes', JSON.stringify([...storedQuotes, quote])) : localStorage.setItem('storedQuotes', JSON.stringify([quote]));    
     storedQuotes ? setSavedQuotes([...storedQuotes, quote]) : setSavedQuotes([quote]);
   }
 
   const removeSavedQuote = (quote) => {
+    quote.isSaved = false;
     const storedQuotes = JSON.parse(localStorage.getItem('storedQuotes'));
-    const filteredQuotes = storedQuotes.filter(storedQuote => storedQuote.quote !== quote)
+    const filteredQuotes = storedQuotes.filter(storedQuote => storedQuote.quote !== quote.quote)
     localStorage.setItem('storedQuotes', JSON.stringify(filteredQuotes))
     setSavedQuotes([...filteredQuotes])
   }
@@ -123,7 +125,8 @@ function App() {
                   <Quote 
                     currentQuestion={currentQuestion} 
                     validStatus={validStatus} 
-                    saveCurrentQuote={saveCurrentQuote} 
+                    saveCurrentQuote={saveCurrentQuote}
+                    removeSavedQuote={removeSavedQuote}  
                     getRandomQuestion={getRandomQuestion}
                   />
                   {feedback && <h2 className={'feedback'}>{feedback}</h2>}
