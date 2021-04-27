b150fa4b15c2ab4c949269220d261c2fff0c9e48
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-27 16:09:46 -0600

    Conduct e2e testing of all features

--- cypress/integration/niche-got-test.js
+++ cypress/integration/niche-got-test.js
@@ -1,72 +1,111 @@
 describe('Rancid Tomatillos', () => {
   beforeEach(() => {
     cy.intercept('https://game-of-thrones-quotes.herokuapp.com/v1/characters', { fixture:'character_obj.json' })
-      .visit.skip('http://localhost:3000/')
+      .visit('http://localhost:3000/')
+    cy.clearLocalStorage()
   });
   
-  it.skip('should display a header, footer, and an image on load', () => {
+  it('should display a header, footer, and an image on load', () => {
     cy.get('header').should('be.visible')
     cy.get('footer').should('be.visible')
     cy.get('img[class=home-img]').should('be.visible')
   });
 
-  it.skip('should display a multiple choice quiz when the play button is clicked', () => {
-    cy.get('a[id=play]')
+  it('should display a multiple choice quiz when the play button is clicked', () => {
+    cy.get('a[id=play-link]')
       .click()
-    cy.contains('h2')
-    cy.get('section[class=answer-section]')
-      .contains('button')
+    cy.get('h2').should('be.visible')
+    cy.get('section.answer-section').children().should('have.length', 4)
   });
 
-  it.skip('should give immediate and accurate feedback when an answer is selected', () => {
-    cy.get('a[id=play]')
+  it('should give immediate and accurate feedback when an selected answer is correct', () => {
+    cy.get('a[id=play-link]')
       .click()
-    cy.get('section[class=quote-section]')
+    cy.get('button.true')
+      .click()
+    cy.get('h2.feedback').should('be.visible')
+    cy.get('h2.feedback').contains('Correct!')
   });
 
-  it.skip('should disable all answer buttons when one is selected', () => {
-
-
+  it('should give immediate and accurate feedback when an selected answer is incorrect', () => {
+    cy.get('a[id=play-link]')
+      .click()
+    cy.get('button.false').first()
+      .click()
+    cy.get('h2.feedback').should('be.visible')
+    cy.get('h2.feedback').contains('Incorrect!')
   });
 
-  it.skip('should allow users to see the next quote', () => {
-    cy.get('a[id=play]')
+
+  it('should disable all answer buttons when one is selected', () => {
+    cy.get('a[id=play-link]')
     .click()
-
+    cy.get('button.false').first()
+      .click()
+    cy.get('h2.feedback').should('be.visible')
+    cy.get('h2.feedback').contains('Incorrect!')
+    cy.get('button.true').should('have.css', 'pointer-events', 'none')
+    cy.get('button.true').should('have.css', 'cursor', 'not-allowed')
   });
 
-  it.skip('should allow users to save a quote of their choosing', () => {
-    cy.get('a[id=play]')
+  it('should allow users to see the next quote', () => {
+    cy.get('a[id=play-link]')
+      .click()
+    cy.get('button.next')
+      .click()
+    cy.get('button.true').should('not.have.class', 'selected')
+    cy.get('button.false').first().should('not.have.class', 'selected')
+  });
+
+  it('should allow users to save a quote of their choosing', () => {
+    cy.get('a[id=play-link]')
+      .click()
+    cy.get('button.save')
+      .click()
+    cy.get('button.save')
+      .contains('Saved')
+  });
+
+  it('should display a list of saved quotes', () => {
+    cy.get('a[id=play-link]')
     .click()
-
+    cy.get('button.save')
+      .click()
+    cy.get('a[id=saved-quotes-link]')
+      .click()
+    cy.get('section.center-piece').children().should('have.length', 1)
   });
 
-  it.skip('should display a list of saved quotes', () => {
-    cy.get('a[id=saved-quotes]')
+  it.only('should allow users to remove a selected quote', () => {
+    cy.get('a[id=play-link]')
+    .click()
+    cy.get('button.save')
       .click()
-
-  });
-
-  it.skip('should allow users to remove a selected quote', () => {
-    cy.get('a[id=saved-quotes]')
+    cy.get('a[id=saved-quotes-link]')
       .click()
-
+    cy.get('button.remove-button')
+      .click()
+    cy.get('section.center-piece').children().should('have.length', 0)
   });
 })
 
 
 describe('Requests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
   it.skip('should reveal an error message when the server returns a 404 status code', () => {
     cy.intercept({
       method: 'GET',
       url: 'https://game-of-thrones-quotes.herokuapp.com/v1/characters'
     },
     {
-      statusCode: 404
+      statusCode: 404,
     })
     
-    cy.visit.skip('http://localhost:3000/')
-    cy.get('h2[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
+    cy.visit('http://localhost:3000/')
+    cy.get('h1[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
   });
 
   it.skip('should reveal an error message when the server returns a 500 status code', () => {
@@ -78,7 +117,7 @@ describe('Requests', () => {
       statusCode: 500
     })
 
-    cy.visit.skip('http://localhost:3000/')
-    cy.get('h2[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
+    cy.visit('http://localhost:3000/')
+    cy.get('h1[class=error-message]').should('contain', 'We\'re sorry, an error has occurred. Please try again later.')
   });
 })
