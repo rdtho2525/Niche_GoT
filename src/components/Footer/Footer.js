a29648642d961043540ebf2b46fc9efed4ab2c03
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-26 18:31:44 -0600

    Apply responsive styling to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,11 @@ const Footer = ({ getRandomQuestion }) => {
 
   return (
     <footer>
-      <Link to="/">Home</Link>
-      <Link to="/play" onClick={() => getRandomQuestion()}>Play</Link>
-      <Link to="/saved-quotes" className="saved-link">Saved Quotes</Link>
+      <div className={"nav-links"}>
+        <Link to="/">Home</Link>
+        <Link to="/play" onClick={() => getRandomQuestion()}>Play</Link>
+        <Link to="/saved-quotes" className="saved-link">Saved Quotes</Link>
+      </div>
     </footer>
   )
 }
